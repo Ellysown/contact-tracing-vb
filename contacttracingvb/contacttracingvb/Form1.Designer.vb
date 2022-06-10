2c3ed2275536e9052b29f9eae5c068114f04862a
Ellyson <xxLAxxjeje09@gmail.com>
2022-06-11 04:23:59 +0800

    removed background image

--- contacttracingvb/contacttracingvb/Form1.Designer.vb
+++ contacttracingvb/contacttracingvb/Form1.Designer.vb
@@ -167,7 +167,6 @@ Partial Class form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(524, 302)
         Me.Controls.Add(Me.PictureBox1)
